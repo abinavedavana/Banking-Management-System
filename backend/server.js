22ac1beee3c28788e923deeda1c7d56fdfb1848e
abinav964 <abinavedavana@gmail.com>
2026-03-19 16:53:52 +0530

    commit changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://banking-management-system-3dbzc8bay-abinavedavanas-projects.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/uploads",express.static(path.join(__dirname,"uploads")));
